bf0a590197e22d9fcd88eec15b69f48a1ebc8e72
fuquanlin <fuquanlin@jk.cn>
2016-10-14 17:29:14 +0800

    add gulp build for UI

---
+++ gulpfile.js
@@ -0,0 +1,124 @@
+var gulp = require('gulp');
+var gutil = require('gulp-util');
+var bower = require('bower');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var minifycss = require('gulp-minify-css');
+var rename = require('gulp-rename');
+var clean = require('gulp-clean');
+var imagemin = require('gulp-imagemin');
+var cache = require('gulp-cache');
+var templateCache = require('gulp-angular-templatecache');
+
+
+var buildConfig = {
+    projectName: 'fishbone',
+    rootPath: 'dist',
+    js_all: [
+        "src/main/resources/static/common/config.js",
+        "src/main/resources/static/common/common.js",
+        "src/main/resources/static/common/service/api.service.js",
+        "src/main/resources/static/common/service/common.service.js",
+        "src/main/resources/static/common/service/user.service.js",
+        "src/main/resources/static/common/service/role.service.js",
+        "src/main/resources/static/common/service/log.service.js",
+        "src/main/resources/static/index.js",
+        "src/main/resources/static/templates/main.js",
+        "src/main/resources/static/templates/welcome/welcome.js",
+        "src/main/resources/static/templates/user/user.js",
+        "src/main/resources/static/templates/role/role.js",
+        "src/main/resources/static/templates/log/log.js"
+    ],
+    template_all: [
+        "src/main/resources/static/templates/*.html",
+        "src/main/resources/static/templates/*/*.html"
+    ],
+    js_lib: [
+        "src/main/resources/static/lib/angular/angular.js",
+        "src/main/resources/static/lib/angular-animate/angular-animate.js",
+        "src/main/resources/static/lib/angular-ui-router/release/angular-ui-router.js",
+        "src/main/resources/static/lib/angular-bootstrap/ui-bootstrap-tpls.js",
+        "src/main/resources/static/lib/ng-file-upload/ng-file-upload-all.min.js",
+        "src/main/resources/static/lib/jquery/dist/jquery.js",
+        "src/main/resources/static/lib/bootstrap/dist/js/bootstrap.min.js",
+        "src/main/resources/static/lib/metisMenu/dist/metisMenu.min.js",
+        "src/main/resources/static/lib/sb-admin/app/js/sb-admin-2.js"
+    ],
+    css_all: [
+        "src/main/resources/static/lib/bootstrap/dist/css/bootstrap.min.css",
+        "src/main/resources/static/lib/metisMenu/dist/metisMenu.min.css",
+        "src/main/resources/static/lib/sb-admin/app/styles/sb-admin-2.css",
+        "src/main/resources/static/lib/font-awesome/css/font-awesome.min.css"
+    ],
+    img: ['src/main/resources/static/assets/imgs/*'],
+
+    clear: [
+        'dist/*'
+    ]
+};
+
+gulp.task('clean', function () {
+        return gulp.src(buildConfig.clear)
+            .pipe(clean({force: true}));
+    }
+);
+
+
+gulp.task('install', function () {
+    return bower.commands.install()
+        .on('log', function (data) {
+            gutil.log('bower', gutil.colors.cyan(data.id), data.message);
+        });
+});
+
+
+// Styles
+gulp.task('styles', function () {
+    return gulp.src(buildConfig.css_all)
+        .pipe(minifycss())
+        .pipe(concat("all.min.css"))
+        .pipe(gulp.dest('dist/assets/css'));
+});
+
+
+// Libs
+gulp.task('libs', function () {
+    return gulp.src(buildConfig.js_all)
+        .pipe(concat('lib.min.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest('dist/lib'));
+});
+
+// Scripts
+gulp.task('scripts', function () {
+    return gulp.src(buildConfig.js_all)
+        .pipe(concat('all.min.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest('dist/scripts'));
+});
+
+// Templates
+gulp.task('templates', function () {
+    gulp.src(buildConfig.template_all)
+        .pipe(templateCache({
+            standalone: true,
+            root: 'templates'
+        }))
+        .pipe(gulp.dest('dist/scripts'));
+
+});
+
+
+// Images
+gulp.task('images', function () {
+    return gulp.src(buildConfig.img)
+        .pipe(cache(imagemin({optimizationLevel: 3, progressive: true, interlaced: true})))
+        .pipe(gulp.dest('dist/assets/imgs'));
+});
+
+// Default task
+gulp.task('default', ['clean'], function () {
+    gulp.start('styles', 'libs', 'scripts', 'images');
+});
+
+
