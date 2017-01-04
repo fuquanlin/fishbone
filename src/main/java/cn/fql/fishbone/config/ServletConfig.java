678183ecb0ae7ca09b0ba3f33c5bae2aa1f9d5e7
fuquanlin <fuquanlin@jk.cn>
2017-01-04 13:51:11 +0800

    add hystrix servlet

---
+++ src/main/java/cn/fql/fishbone/config/ServletConfig.java
@@ -0,0 +1,23 @@
+package cn.fql.fishbone.config;
+
+import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * metrics URL is /metrics/hystrix.stream
+ * Created by fuquanlin on 2017/1/3.
+ */
+@Configuration
+public class ServletConfig {
+
+    @Bean(name = "hystrixRegistrationBean")
+    public ServletRegistrationBean servletRegistrationBean() {
+        ServletRegistrationBean registration = new ServletRegistrationBean(
+                new HystrixMetricsStreamServlet(), "/metrics/hystrix.stream");
+        registration.setName("hystrixServlet");
+        registration.setLoadOnStartup(1);
+        return registration;
+    }
+}
